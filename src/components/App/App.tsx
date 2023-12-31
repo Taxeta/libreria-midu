3451df1e4c3f1081d6124a0d1c044f8a38acc31c
Jose Manuel Vera <josemanuelvera1991@gmail.com>
2023-12-31 14:57:07 +0100

    Add mobileNavigation and path routes on navigate bar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,18 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "../../Pages/HomePage/HomePage";
 import LibraryPage from "../../Pages/LibraryPage/LibraryPage";
+import MobileNavigationBar from "../MobileNavigationBar/MobileNavigationBar";
+import paths from "../../paths/paths";
 
 const App = (): React.ReactElement => {
   return (
     <main>
       <HomePage />
-      <LibraryPage />
+      <Routes>
+        <Route path={paths.root} element={<Navigate to={paths.home} />} />
+        <Route path={paths.home} element={<LibraryPage />} />
+      </Routes>
+      <MobileNavigationBar />
     </main>
   );
 };
---
+++ src/paths/paths.ts
@@ -0,0 +1,10 @@
+import { PathsStructure } from "./types";
+
+const paths: PathsStructure = {
+  root: "/",
+  home: "/home",
+  contact: "/contact",
+  readingList: "/reading-list",
+};
+
+export default paths;
